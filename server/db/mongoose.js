94a595b1b03553bd43b4cc10f07e559e60e791d2
Vaibhav Yadav <imvaibhavyadav@gmail.com>
2019-01-29 01:38:59 +0530

    with heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Mytodo');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Mytodo');
 // [^] Mongos Config Code
 
 module.exports = {mongoose};  //link
